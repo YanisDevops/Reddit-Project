9485264e7192bfcbaf1e635585da3f004c2325b3
YanisDevops <142588782+YanisDevops@users.noreply.github.com>
2024-03-09 23:00:55 +0100

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "tetris-bucket"
+    bucket         = "ybdatastore"
     region         = "us-east-1"
     key            = "EKS-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
+    #dynamodb_table = "Lock-Files"
+    #encrypt        = true
   }
   required_version = ">=0.13.0"
   required_providers {
